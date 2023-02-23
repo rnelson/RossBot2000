3df2dbb9378d5f7e6df1a2715acc01b90174c9be
Ross Nelson <ross.nelson@gmail.com>
2023-02-23 16:55:39 -0600

    Make winners configurable

--- Modules/DiceModule.cs
+++ Modules/DiceModule.cs
@@ -5,13 +5,36 @@ namespace RossBot2000.Modules;
 
 [SuppressMessage("ReSharper", "UnusedType.Global")]
 [SuppressMessage("ReSharper", "UnusedMember.Global")]
+[SuppressMessage("ReSharper", "PrivateFieldCanBeConvertedToLocalVariable")]
 public class DiceModule : ModuleBase<SocketCommandContext>
 {
+	private readonly IConfiguration _configuration; 
 	private readonly Random _random = new();
-	private readonly ulong[] Winners =
+	private readonly List<ulong> Winners;
+
+	public DiceModule(IConfiguration configuration)
 	{
-		329744102189039618UL // Ross
-	};
+		_configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+
+		Winners = new List<ulong>();
+		
+		try
+		{
+			var ids = _configuration
+				.GetSection("Dice")
+				.GetSection("Winners")
+				.Get<ulong[]>();
+
+			if (ids is null)
+				throw new Exception("no winners found");
+			
+			Winners.AddRange(ids.ToList());
+		}
+		catch
+		{
+			Winners.Add(329744102189039618UL); // Ross
+		}
+	}
 	
 	[Command("SavingThrow")]
 	[Alias("d20st")]
