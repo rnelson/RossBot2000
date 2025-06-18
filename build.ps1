d0ce722cce7c3b4aad332da281f51d720fce74a7
Ross Nelson <ross.nelson@gmail.com>
2025-06-17 20:53:54 -0500

    Add a build script

---
+++ build.ps1
@@ -0,0 +1,7 @@
+dotnet build src/RossBot2000.sln
+Remove-Item -Recurse -Force dist
+New-Item -ItemType Directory -Path dist
+Copy-Item -Path src/RossBot2000/bin/Debug/net9.0/* -Destination dist -Recurse
+Get-ChildItem src/Modules | ForEach-Object {
+	Copy-Item -Path (Get-ChildItem -Path (Join-Path -Path $_.FullName -ChildPath "bin/Debug/net9.0") -Filter "*Module.dll").FullName -Destination dist
+}
